6660606a261e6a9a66d18235545f2c72c16eb30c
NikaMaxo7 <makhoshvilinika13@gmail.com>
2025-02-26 08:06:01 -0800

    third commit

---
+++ level 03/classwork/classwork.py
@@ -0,0 +1,20 @@
+# #N1 classwork
+# num1 = input("enter first number: ")
+# num2 = input("enter second number: ")
+# num3 = input("enter third number: ")
+# answer = int(num1) + int(num2) + int(num3)
+# print("answer: " + str(answer))
+
+# #N2 classwork
+# name = input("enter your name: ")
+# print("hello, " + name + "!")
+
+# #N3 classwork
+# num1 = input("enter first number: ")
+# num2 = input("enter second number: ")
+# print(num1 > num2)
+
+#N4 classwork
+word = input("enter word: ")
+n = input("enter amount: ")
+print(word * int(n))
